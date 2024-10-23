bca2d4768c763a2254fc785eb3e1eb6e755252ab
Alban Bajrami <albanba@uio.no>
2024-10-23 14:49:48 +0200

    Implemented admin functionalities such as unassign_role() and clear_messages()

--- cogs/administrator.py
+++ cogs/administrator.py
@@ -35,7 +35,7 @@ class Administrator(commands.Cog):
         if user is None or '':
 
             members_count = 0
-
+            # await interaction.response.defer()
             for member in interaction.guild.members:
                 print(member)
                 if role not in member.roles:
@@ -45,7 +45,7 @@ class Administrator(commands.Cog):
                     except discord.Forbidden:
                         await interaction.followup.send("Could not assign role. Missing permission.", ephemeral=True)
 
-            await interaction.response.send_message(f"Assigned role: {role}")
+            await interaction.response.send_message(f"Assigned role: {role} to everyone")
 
         else:
            
@@ -58,9 +58,75 @@ class Administrator(commands.Cog):
                     except discord.Forbidden:
                         await interaction.followup.send("Could not assign role. Missing permission.", ephemeral=True)
 
-        await interaction.response.send_message(f"Assigned role: {role}")
+        await interaction.response.send_message(f"Assigned role: {role} to user {user}")
 
+
+    @app_commands.command(name="unassign_role", description="Remove user(s) specific server role")
+    @app_commands.checks.bot_has_permissions(administrator=True)
+    async def unassign_role(self, interaction: discord.Interaction, role_name: str, user: str):
+        """
+        Remove role from a user.
+
+        args: 
+            - self
+            - interaction: discord.Interaction
+            - role_name: str
+            - user: str
+
+        returns:
+            - None
         
+        """
+        role = discord.utils.get(interaction.guild.roles, name=role_name)
+
+        if role is None:
+            await interaction.response.send_message(f"Role {role_name} does not exist.", ephemeral=True)
+            return
+
+        member = discord.utils.find(lambda m: m.name.lower() == user.lower(), interaction.guild.members)
+        print(member)
+        if member:
+            if role in member.roles:
+                try:
+                    await member.remove_roles(role)
+                    print(f"removed role {role} from user {user}")
+                    await interaction.response.send_message(f"Unassigned role: {role} from user {user}")
+                except discord.Forbidden:
+                    await interaction.followup.send("Could not assign role. Missing permission.", ephemeral=True)
+        
+        else:
+            await interaction.response.send_message(f"User {user} does not exist.", ephemeral=True)
+            return
+
+
+    @app_commands.command(name="clear_messages", description="Delete specified amount of messages in a channel")
+    @app_commands.checks.bot_has_permissions(administrator=True)
+    async def clear_messages(self, interaction: discord.Interaction, amount: int):
+        """
+        Clear messages in a channel.
+
+        Limit amount between 1-100 to delete messages.
+
+        args: 
+            - self
+            - interaction: discord.Interaction
+            - amount: int
+
+        returns:
+            - None
+        
+        """
+        
+        if amount == 0:
+            await interaction.response.send_message("No amount was entered.")
+            return
+
+        if amount < 1 or amount > 100:
+            await interaction.response.send_message("Amount has a limit between 1-100 to delete messages.")
+
+        delete = await interaction.channel.purge(limit=amount)
+
+        await interaction.response.send_message(f"Successfully deleted {amount} messages.")
 
 async def setup(bot: commands.Bot):
     """
